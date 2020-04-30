debfde03669e23436a83a25bfed9b67ced4bb275
Gilbert King <gilbertking91@gmail.com>
2020-04-30 17:55:10 -0500

    cleanup

---
+++ client/index.js
@@ -0,0 +1,26 @@
+import Layout from '../components/layout'
+import Header from '../components/header'
+import SubmitForm from '../components/form'
+import Organizers from '../components/organizers'
+import WhatToExpect from '../components/what-to-expect'
+import Description from '../components/description'
+import Footer from '../components/footer'
+import GroupBanner from '../components/group-banner'
+import Tagline from '../components/tagline'
+import Navbar from '../components/navbar'
+
+
+export default () => (
+  <Layout>
+    <Navbar />
+    <Header />
+    <Tagline />
+    <GroupBanner />
+    <Description />
+    <SubmitForm />
+    <WhatToExpect />
+    <Organizers />
+    <Footer />
+  </Layout>
+)
+
